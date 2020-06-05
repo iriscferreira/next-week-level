1fcb0897d51b907ada7837bdc1c84ebe2a4a05f4
Iris Ferreira <irisferreira@poli.ufrj.br>
2020-06-05 01:42:57 -0300

    create route for items

--- src/controllers/ItemsController.ts
+++ src/controllers/ItemsController.ts
@@ -3,7 +3,7 @@ import knex from '../database/connection';
 
 class ItemsController {
 
-    async create(request: Request, response: Response) {
+    async index(request: Request, response: Response) {
         const items = await knex('items').select('*');
 
         const serializedItems = items.map(item => {
--- src/routes.ts
+++ src/routes.ts
@@ -4,13 +4,18 @@ import knex from './database/connection';
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
 
+// index, show, create, update, delete
+
 const routes = express.Router();
 
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
-routes.get('/items', itemsController.create);
+routes.get('/items', itemsController.index);
 
 routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
 
 export default routes;
